7e6c44592ce9b3dc64e91f63a1bb52044b8aca06
raminarmanfar <ramin.armanfar@gmail.com>
2018-01-25 11:39:21 +0100

    'Bean

--- src/org/ramin/javabrains/DrawingApp.java
+++ src/org/ramin/javabrains/DrawingApp.java
@@ -1,7 +1,7 @@
 package org.ramin.javabrains;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
@@ -9,9 +9,11 @@ public class DrawingApp {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// Triangle triangle = new Triangle();
-		ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring.xml"});
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring.xml"});
+		context.registerShutdownHook();
 		BeanFactory factory = context;
 		Triangle triangle = (Triangle) factory.getBean("triangle1");
 		triangle.draw();
+		
 	}
 }
